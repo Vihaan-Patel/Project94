84cd305a40607ff9dcdaf2c9c954ad3acd8bd436
Vihaan-Patel <92143558+Vihaan-Patel@users.noreply.github.com>
2022-01-05 09:44:14 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+const firebaseConfig = {
+    apiKey: "AIzaSyAHTNtCakWD04DJtFvygZ9cbRQoEWqvkqk",
+    authDomain: "kwitter-dc68a.firebaseapp.com",
+    databaseURL: "https://kwitter-dc68a-default-rtdb.firebaseio.com",
+    projectId: "kwitter-dc68a",
+    storageBucket: "kwitter-dc68a.appspot.com",
+    messagingSenderId: "295286954100",
+    appId: "1:295286954100:web:2dc330e8a3639295d35921",
+    measurementId: "G-C5G8BRY98Z"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
